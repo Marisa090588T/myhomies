aeacacad3c0880798e752f9b4c8f5acb99c3d571
lodi palle <lodipalle@gmail.com>
2020-09-15 14:57:32 +1000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: :show
+
+  resources :homes, only: [:create, :edit, :update] do
+    resources :expenses, only: [:create, :new, :index, :show] do
+      resources :expense_shares, only: :update
+    end 
+
+  #   resources :chores, only: [:create, :new, :index, :show] do
+  #     patch :done, on: :member
+  #   end
+  end
+
+  # resources :expense_share, only: [] do
+  #   patch :check, on: :member
+  # end
+
 end
