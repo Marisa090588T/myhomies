40b0136646191a0b06d48980f82109893c394654
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-09-17 16:31:59 +1000

    fixed new chores form - added field to select homies to assign

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,51 +1,53 @@
 class ChoresController < ApplicationController
-  def index
-    @chores = Chore.all
-    @home = Home.find(params[:home_id])
-    @chore = Chore.find_by(assignee: current_user, home: @home)
-  end
+ def index
+   @chores = Chore.all
+   @home = Home.find(params[:home_id])
+   @chore = Chore.find_by(assignee: current_user, home: @home)
+ end
 
-  def new
-    @home = Home.find(params[:home_id])
-    @chore = Chore.new
-    @chore.home = @home
-  end
+ def new
+   @home = Home.find(params[:home_id])
+   @chore = Chore.new
+   @chore.home = @home
 
-  def create
-    @home = Home.find(params[:home_id])
-    @chore = Chore.new(chore_params)
-    @chore.home = @home
-    @chore.completed = false
-    @chore.assignee = @home.users.sample
-    
-    if @chore.save
-        redirect_to home_chores_path, notice: 'Chore was successfully listed!'    
-    else
-      render :new
-    end
-  end
-    
-  def show
-    @home = Home.find(params[:home_id])
-    @chore = Chore.find(params[:id])
-    @chore.home = @home
-  end
+   @homies = []
+   @home.users.each do |homie|
+     @homies << homie.first_name
+   end
+ end
 
-  private
+ def create
+   @home = Home.find(params[:home_id])
+   @chore = Chore.new(chore_params)
+   @chore.home = @home
+   @chore.completed = false
+   @chore.assignee = @home.users.sample
 
-  def chore_params
-    params.require(:chore).permit(:name, :description, :assignee)
-  end
+   if @chore.save
+       redirect_to home_chores_path, notice: 'Chore was successfully listed!'
+   else
+     render :new
+   end
+ end
 
-  def complete
-    @home = Home.find(params[:home_id])
-    @chore = Chore.find(params[:id])
-    @home.chore = @home
-    @chore.completed = true
-    @chore.save
-    redirect_to dashboard_path # TODO: should go back show page?
-  end
+ def show
+   @home = Home.find(params[:home_id])
+   @chore = Chore.find(params[:id])
+   @chore.home = @home
+ end
+
+ private
+
+ def chore_params
+   params.require(:chore).permit(:name, :description, :assignee)
+ end
+
+ def complete
+   @home = Home.find(params[:home_id])
+   @chore = Chore.find(params[:id])
+   @home.chore = @home
+   @chore.completed = true
+   @chore.save
+   redirect_to dashboard_path # TODO: should go back show page?
+ end
 end
-
-# change status as mark (conplete or not)
-# need a update?
