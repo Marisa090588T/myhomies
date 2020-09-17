2908df03ec13c4124983e98549985539fdec9208
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-09-17 13:36:18 +1000

    fixed chatroom bug - chatroom creates upon home creation now, added chatroom link to navbar

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomsController < ApplicationController
-  def show
-    @chatroom = Chatroom.find(params[:id])
-    @message = Message.new
-  end
-
+ def show
+   @home = Home.find(params[:id])
+   @chatroom = Chatroom.find(@home.id)
+   @message = Message.new
+ end
 end
