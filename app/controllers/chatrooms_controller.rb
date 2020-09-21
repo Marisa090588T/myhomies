de15ba30878134fa811d6cd4b6e8e57f8619c166
Sergey Osipov <serge@osipoffphoto.com>
2020-09-21 22:32:11 +1000

    New cards on dashboard

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomsController < ApplicationController
- def show
-   @home = Home.find(params[:id])
-   @chatroom = Chatroom.find(@home.id)
-   @message = Message.new
- end
+  def show
+    @home = Home.find(params[:id])
+    @chatroom = Chatroom.find(@home.id)
+    @message = Message.new
+  end
 end
