ce397cb8c2e446a030935fc48f89cd3fa7cf7e83
Marisa Tsubokura <marieee090530@gmail.com>
2020-09-22 11:41:33 +1000

    expenses controller, index pages, individual index pages

--- app/controllers/expense_shares_controller.rb
+++ app/controllers/expense_shares_controller.rb
@@ -1,42 +1,37 @@
-class ExpenseSharesController < ApplicationController
-  def update
-    @expense_share = ExpenseShare.find(params[:id])
-    @expense = Expense.find(params[:expense_id])
-    @expense_share.expense = @expense
+# class ExpenseSharesController < ApplicationController
+#   def update
+#     @expense_share = ExpenseShare.find(params[:id])
+#     @expense = Expense.find(params[:expense_id])
+#     @expense_share.expense = @expense
 
-    if @expense_share.update(expense_share_params)
-      redirect_to home_expense_path(@home, @expense), notice: 'Bill status was updated.'
-    end
-  end
+#     if @expense_share.update(expense_share_params)
+#       redirect_to home_expense_path(@home, @expense), notice: 'Bill status was updated.'
+#     end
+#   end
 
-  def index
-    @home = Home.find(params[:home_id])
-    @expense_shares = current_user.expense_shares
-  end
+#   def index
+#     @home = Home.find(params[:home_id])
+#     @expense_shares = current_user.expense_shares
+#   end
 
-  def show
-    @home = Home.find(params[:home_id])
-    @expense = Expense.find(params[:id])
-    @expense_share = ExpenseShare.find_by(user: current_user, expense: @expense)
-  end
+#   def show
+#     @home = Home.find(params[:home_id])
+#     @expense = Expense.find(params[:id])
+#     @expense_share = ExpenseShare.find_by(user: current_user, expense: @expense)
+#   end
 
-  def paid
-    @expense_share = ExpenseShare.find(params[:id])
-    @expense = Expense.find(params[:expense_id])
-    @expense_share.expense = @expense
-    @expense_share.paid = true
-    @expense_share.save
-    redirect_to dashboard_path
-  end
+#   def paid
+#     @expense_share = ExpenseShare.find(params[:id])
+#     @expense = Expense.find(params[:expense_id])
+#     @expense_share.expense = @expense
+#     @expense_share.paid = true
+#     @expense_share.save
+#     redirect_to dashboard_path
+#   end
 
-  private
+#   private
 
-  def expense_shares_params
-    params.require(:expense_shares).permit(:paid) 
-  end
-end
-
-
-# all bills should be displayed in each dashboard
-# (show or index) in the controller is needed?
-# change status as mark(paid or unpaid)
+#   def expense_shares_params
+#     params.require(:expense_shares).permit(:paid) 
+#   end
+# end
--- app/controllers/user/expenses_controller.rb
+++ app/controllers/user/expenses_controller.rb
@@ -1,6 +1,9 @@
 class User::ExpensesController < ApplicationController
   def index
-    @expenses = current_user.expenses
     @home = current_user.home
+    @expenses = @home.expenses.all.order(:created_at)
+    @user = current_user
+    @expense = Expense.find_by(user: current_user, home: @home)
+    @expense_share = ExpenseShare.find_by(user: @user, expense: @expense)
   end
 end
