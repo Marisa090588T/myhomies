8bd2433b49b6f2a2eb3846b75b4b4b3aa7df3133
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-09-24 16:43:01 +1000

    added flatpickr css import

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -1,5 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/themes/dark.css";
+import '../node_modules/flatpickr/dist/flatpickr.min.css';
+
 
 const initCalendar = () => {
   const dateFlatpickr = document.querySelector("#date-flatpickr");
